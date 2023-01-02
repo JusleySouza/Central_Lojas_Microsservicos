6177512773d3a1a7849ed16034536a910e2ef92d
ArturStefan <arthurstephan13@gmail.com>
2023-01-02 19:23:42 -0300

    Criação de busca por rg

--- backend/CentralStoresEmployees/src/main/java/com/central/stores/employees/repository/EmployeesRepository.java
+++ backend/CentralStoresEmployees/src/main/java/com/central/stores/employees/repository/EmployeesRepository.java
@@ -9,6 +9,7 @@ import com.central.stores.employees.model.Employee;
 
 public interface EmployeesRepository extends JpaRepository<Employee, UUID> {
 	Employee findByCpf(String cpf);
+	Employee findByRg(String rg);
 	List<Employee> findAllByActiveTrue();
 	List<Employee> findAllByActiveTrueAndAddressNeighborhood(String neighborhood);
 }
