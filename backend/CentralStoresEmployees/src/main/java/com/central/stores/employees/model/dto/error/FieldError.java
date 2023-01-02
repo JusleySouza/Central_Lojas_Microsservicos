3b8f9215240e5cbc7b6c5a174c8fe4e85b106bdf
ArturStefan <arthurstephan13@gmail.com>
2023-01-02 18:00:56 -0300

    Objeto par apadronização de resposta de erros de validação

---
+++ backend/CentralStoresEmployees/src/main/java/com/central/stores/employees/model/dto/error/FieldError.java
@@ -0,0 +1,15 @@
+package com.central.stores.employees.model.dto.error;
+
+import lombok.Data;
+
+@Data
+public class FieldError {
+
+	private String field;
+	private String message;
+	
+	public FieldError(String field, String message) {
+		this.field = field;
+		this.message = message;
+	}
+}
---
+++ backend/CentralStoresEmployees/src/main/java/com/central/stores/employees/model/dto/error/ResponseError.java
@@ -0,0 +1,33 @@
+package com.central.stores.employees.model.dto.error;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import javax.validation.ConstraintViolation;
+
+import lombok.Data;
+
+@Data
+public class ResponseError {
+
+	private final String MESSAGE = "Validation error";
+	private Collection<FieldError> errors;
+
+	public ResponseError(Collection<FieldError> errors) {
+		this.errors = errors;
+	}
+
+	public static <T> ResponseError createFromValidation(Set<ConstraintViolation<T>> violations) {
+
+		List<FieldError> errors = violations
+			.stream()
+			.map(violation -> new FieldError(violation.getPropertyPath().toString(), violation.getMessage()))
+			.collect(Collectors.toList());
+		
+		ResponseError responseError = new ResponseError(errors);
+		
+		return responseError;
+	}
+}
