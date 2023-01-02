d5a167528d944a04162a70dc548f0fcea5470cf3
ArturStefan <arthurstephan13@gmail.com>
2023-01-02 17:58:16 -0300

    Padronização da resposta de erro

---
+++ backend/CentralStoresEmployees/src/main/java/com/central/stores/employees/exception/ExceptionResponse.java
@@ -0,0 +1,27 @@
+package com.central.stores.employees.exception;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import lombok.Getter;
+
+@Getter
+public class ExceptionResponse implements Serializable {
+	
+	private static final long serialVersionUID = 1l;
+	
+	protected Date timestamp;
+	protected String message;
+	protected String details;
+	
+	public ExceptionResponse() {
+	}
+
+	public ExceptionResponse(Date timestamp, String message, String details) {
+		super();
+		this.timestamp = timestamp;
+		this.message = message;
+		this.details = details;
+	}
+
+}
