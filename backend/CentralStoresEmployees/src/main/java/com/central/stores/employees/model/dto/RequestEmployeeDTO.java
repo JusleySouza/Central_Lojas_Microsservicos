88afa94682ba5cad55e53c9800fd8060aec9f51b
ArturStefan <arthurstephan13@gmail.com>
2023-01-02 17:55:16 -0300

    Adição de validação de campos

--- backend/CentralStoresEmployees/src/main/java/com/central/stores/employees/model/dto/RequestEmployeeDTO.java
+++ backend/CentralStoresEmployees/src/main/java/com/central/stores/employees/model/dto/RequestEmployeeDTO.java
@@ -1,5 +1,8 @@
 package com.central.stores.employees.model.dto;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -14,11 +17,19 @@ import lombok.ToString;
 @NoArgsConstructor
 @AllArgsConstructor
 public class RequestEmployeeDTO {
+	@NotEmpty(message = "{name.not.empty}")
 	private String name;
+	@NotEmpty(message = "{cpf.not.empty}")
 	private String cpf;
+	@NotEmpty(message = "{rg.not.empty}")
 	private String rg;
+	@NotEmpty(message = "{role.not.empty}")
 	private String role;
+	@NotEmpty(message = "{gender.not.empty}")
 	private String gender;
+	@NotEmpty(message = "{phone.not.empty}")
 	private String phone;
+	@Email(message = "{email.not.valid}")
+	@NotEmpty(message = "{email.not.empty}")
 	private String email;
 }
