5caf37ed20918be8fa70803c891f264af7d48507
ArturStefan <arthurstephan13@gmail.com>
2023-01-02 17:56:14 -0300

    Adição de validação

--- backend/CentralStoresEmployees/src/main/java/com/central/stores/employees/services/implement/AddressServicesImplent.java
+++ backend/CentralStoresEmployees/src/main/java/com/central/stores/employees/services/implement/AddressServicesImplent.java
@@ -1,21 +1,27 @@
 package com.central.stores.employees.services.implement;
 
+import java.util.Set;
 import java.util.UUID;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validator;
 
 import com.central.stores.employees.config.LoggerConfig;
+import com.central.stores.employees.exception.ResourceNotFoundException;
 import com.central.stores.employees.mapper.AddressMapper;
 import com.central.stores.employees.mapper.UpdateModel;
 import com.central.stores.employees.model.Address;
 import com.central.stores.employees.model.Employee;
 import com.central.stores.employees.model.dto.AddressDTO;
+import com.central.stores.employees.model.dto.error.ResponseError;
 import com.central.stores.employees.repository.AddressRepository;
 import com.central.stores.employees.repository.EmployeesRepository;
 import com.central.stores.employees.services.AddressServices;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 
 @Component
 public class AddressServicesImplent implements AddressServices {
@@ -27,13 +33,22 @@ public class AddressServicesImplent implements AddressServices {
 
 	@Autowired
 	private EmployeesRepository employeesRepository;
+	
+	@Autowired
+	private Validator validator;
 
 	private Address address;
 	
 	private Employee employee;
 	
 	@Override
-	public ResponseEntity<Address> create(AddressDTO requestAddressDTO, UUID employeeId) {
+	public ResponseEntity<Object> create(AddressDTO requestAddressDTO, UUID employeeId) {
+		Set<ConstraintViolation<AddressDTO>> violations = validator.validate(requestAddressDTO);
+		
+		if(!violations.isEmpty()) {
+			return new ResponseEntity<Object>(ResponseError.createFromValidation(violations), HttpStatus.UNPROCESSABLE_ENTITY);
+		}
+		
 		address = mapper.toModel(requestAddressDTO);
 		addressRepository.save(address);
 		
@@ -42,21 +57,29 @@ public class AddressServicesImplent implements AddressServices {
 		
 		employeesRepository.save(employee);
 		
-		LoggerConfig.LOGGER_ADDRESS.info("Endereço do funcionário " + employee.getName() + " salvo com sucesso!!!");
+		LoggerConfig.LOGGER_ADDRESS.info("Address saved");
 
-		return new ResponseEntity<Address>(address, HttpStatus.CREATED);
+		return new ResponseEntity<Object>(address, HttpStatus.CREATED);
 	}
 
 	@Override
-	public ResponseEntity<Address> update(AddressDTO requestAddressDTO, UUID addressId) {
-		address = addressRepository.findById(addressId).get();
+	public ResponseEntity<Object> update(AddressDTO requestAddressDTO, UUID addressId) {
+		Set<ConstraintViolation<AddressDTO>> violations = validator.validate(requestAddressDTO);
+		
+		if(!violations.isEmpty()) {
+			return new ResponseEntity<Object>(ResponseError.createFromValidation(violations), HttpStatus.UNPROCESSABLE_ENTITY);
+		}
+		
+		address = addressRepository.findById(addressId).orElseThrow(() -> 
+		 new ResourceNotFoundException("No records found for this id"));
+		
 		address =  UpdateModel.address(address, requestAddressDTO);
 		
 		addressRepository.save(address);
 		
-		LoggerConfig.LOGGER_ADDRESS.info("Endereço atualizado com sucesso!!!");
+		LoggerConfig.LOGGER_ADDRESS.info("Address updated");
 		
-		return new ResponseEntity<Address>(address, HttpStatus.OK);
+		return new ResponseEntity<Object>(address, HttpStatus.NO_CONTENT);
 	}
 
 }
--- backend/CentralStoresEmployees/src/main/java/com/central/stores/employees/services/implement/EmployeesServicesImplement.java
+++ backend/CentralStoresEmployees/src/main/java/com/central/stores/employees/services/implement/EmployeesServicesImplement.java
@@ -1,22 +1,29 @@
 package com.central.stores.employees.services.implement;
 
 import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validator;
 
 import com.central.stores.employees.config.LoggerConfig;
 import com.central.stores.employees.crypto.Cryptography;
+import com.central.stores.employees.exception.ResourceNotFoundException;
 import com.central.stores.employees.mapper.EmployeeMapper;
 import com.central.stores.employees.mapper.UpdateModel;
 import com.central.stores.employees.model.Employee;
 import com.central.stores.employees.model.dto.RequestEmployeeDTO;
 import com.central.stores.employees.model.dto.ResponseEmployeeDTO;
+import com.central.stores.employees.model.dto.error.ResponseError;
 import com.central.stores.employees.repository.EmployeesRepository;
 import com.central.stores.employees.services.EmployeesServices;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 
 @Component
 public class EmployeesServicesImplement implements EmployeesServices {
@@ -25,46 +32,66 @@ public class EmployeesServicesImplement implements EmployeesServices {
 
 	@Autowired
 	private EmployeesRepository repository;
+	
+	@Autowired
+	private Validator validator;
 
 	private Employee employee;
 
 	private ResponseEmployeeDTO responseEmployeeDTO;
 	
 	@Override
-	public ResponseEntity<List<Employee>> findAll() {
+	@Cacheable(cacheNames = "Employees", key="#root.method.name")
+	public List<Employee> findAll() {
 		List<Employee> listEmployees = repository.findAllByActiveTrue();
 		
 		listEmployees.forEach(employee -> employee = Cryptography.decode(employee));
 
-		LoggerConfig.LOGGER_EMPLOYEE.info("Listagem de funcionários realizada com sucesso!!!");
+		LoggerConfig.LOGGER_EMPLOYEE.info("Employee listing");
 
-		return new ResponseEntity<List<Employee>>(listEmployees, HttpStatus.OK);
+		return listEmployees;
 	}
 
 	@Override
-	public ResponseEntity<Employee> findByCpf(String employeeCpf) {
+	@Cacheable(cacheNames = "Employees", key="#employeeCpf")
+	public Employee findByCpf(String employeeCpf) {
 		employee = repository.findByCpf(Cryptography.encodeCpf(employeeCpf));
+		
+		if(employee == null) {
+			throw new ResourceNotFoundException("No record found for this cpf");
+		}
 
 		employee = Cryptography.decode(employee);
 		
-		LoggerConfig.LOGGER_EMPLOYEE.info("Busca por funcionário " + employee.getName() + " realizada com sucesso!!!");
+		LoggerConfig.LOGGER_EMPLOYEE.info("Employee found");
 
-		return new ResponseEntity<Employee>(employee, HttpStatus.OK);
+		return employee;
 	}
 
 	@Override
-	public ResponseEntity<List<Employee>> findByNeighborhood(String neighborhood) {
+	@Cacheable(cacheNames = "Employees", key="#neighborhood")
+	public List<Employee> findByNeighborhood(String neighborhood) {
 		List<Employee> listEmployees = repository.findAllByActiveTrueAndAddressNeighborhood(neighborhood);
 
+		if(listEmployees.isEmpty()) {
+			throw new ResourceNotFoundException("No record found for this neighborhood");
+		}
+		
 		listEmployees.forEach(employee -> employee = Cryptography.decode(employee));
 		
-		LoggerConfig.LOGGER_EMPLOYEE.info("Listagem de funcionários por bairro realizada com sucesso!!!");
+		LoggerConfig.LOGGER_EMPLOYEE.info("List of employees by neighborhood");
 
-		return new ResponseEntity<List<Employee>>(listEmployees, HttpStatus.OK);
+		return listEmployees;
 	}
 
 	@Override
-	public ResponseEntity<ResponseEmployeeDTO> create(RequestEmployeeDTO requestEmployeeDTO) {
+	public ResponseEntity<Object> create(RequestEmployeeDTO requestEmployeeDTO) {
+		Set<ConstraintViolation<RequestEmployeeDTO>> violations = validator.validate(requestEmployeeDTO);
+		
+		if(!violations.isEmpty()) {
+			return new ResponseEntity<Object>(ResponseError.createFromValidation(violations), HttpStatus.UNPROCESSABLE_ENTITY);
+		}
+		
 		employee = mapper.toModel(requestEmployeeDTO);
 		employee = Cryptography.encode(employee);
 		
@@ -72,15 +99,23 @@ public class EmployeesServicesImplement implements EmployeesServices {
 
 		responseEmployeeDTO = mapper.modelToResponseEmployeeDTO(employee);
 
-		LoggerConfig.LOGGER_EMPLOYEE.info("Funcionário " + employee.getName() + " salvo com sucesso!!!");
+		LoggerConfig.LOGGER_EMPLOYEE.info("Employee saved");
 
-		return new ResponseEntity<ResponseEmployeeDTO>(responseEmployeeDTO, HttpStatus.CREATED);
+		return new ResponseEntity<Object>(responseEmployeeDTO, HttpStatus.CREATED) ;
 
 	}
 
 	@Override
-	public ResponseEntity<ResponseEmployeeDTO> update(RequestEmployeeDTO requestEmployeeDTO, UUID employeeId) {
-		employee = repository.findById(employeeId).get();
+	public ResponseEntity<Object> update(RequestEmployeeDTO requestEmployeeDTO, UUID employeeId) {
+		Set<ConstraintViolation<RequestEmployeeDTO>> violations = validator.validate(requestEmployeeDTO);
+		
+		if(!violations.isEmpty()) {
+			return new ResponseEntity<Object>(ResponseError.createFromValidation(violations), HttpStatus.UNPROCESSABLE_ENTITY);
+		}
+		
+		employee = repository.findById(employeeId).orElseThrow(() -> 
+		 new ResourceNotFoundException("No records found for this id"));
+
 		employee = UpdateModel.employee(employee, requestEmployeeDTO);
 		employee = Cryptography.encode(employee);
 		
@@ -88,20 +123,20 @@ public class EmployeesServicesImplement implements EmployeesServices {
 
 		responseEmployeeDTO = mapper.modelToResponseEmployeeDTO(employee);
 		
-		LoggerConfig.LOGGER_EMPLOYEE.info("Dados do funcionário " + employee.getName() + " atualizados com sucesso!!!");
+		LoggerConfig.LOGGER_EMPLOYEE.info("Employee data successfully updated");
 
-		return new ResponseEntity<ResponseEmployeeDTO>(responseEmployeeDTO, HttpStatus.OK);
+		return new ResponseEntity<Object>(responseEmployeeDTO, HttpStatus.NO_CONTENT) ;
 	}
 
 	@Override
-	public ResponseEntity<ResponseEmployeeDTO> delete(UUID employeeId) {
+	public Employee delete(UUID employeeId) {
 		employee = repository.findById(employeeId).get();
 		employee = mapper.employeeDelete(employee);
 
 		repository.save(employee);
 
-		LoggerConfig.LOGGER_EMPLOYEE.info("Funcionário " + employee.getName() + " deletado com sucesso!!!");
-
-		return new ResponseEntity<ResponseEmployeeDTO>(HttpStatus.NO_CONTENT);
+		LoggerConfig.LOGGER_EMPLOYEE.info("Successfully deleted employee");
+		
+		return employee;
 	}
 }
