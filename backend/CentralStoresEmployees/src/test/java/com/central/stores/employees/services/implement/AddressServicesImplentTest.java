63ff2392521ef6ebedfd534a5aa49073fbd15cda
ArturStefan <arthurstephan13@gmail.com>
2023-01-03 23:03:54 -0300

    Adição dos testes da exceptions

--- backend/CentralStoresEmployees/src/test/java/com/central/stores/employees/services/implement/AddressServicesImplentTest.java
+++ backend/CentralStoresEmployees/src/test/java/com/central/stores/employees/services/implement/AddressServicesImplentTest.java
@@ -1,18 +1,19 @@
 package com.central.stores.employees.services.implement;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 import java.util.Optional;
+import java.util.Set;
 import java.util.UUID;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validator;
 
+import com.central.stores.employees.exception.ResourceNotFoundException;
 import com.central.stores.employees.mapper.AddressMapper;
 import com.central.stores.employees.model.Address;
 import com.central.stores.employees.model.Employee;
@@ -21,9 +22,21 @@ import com.central.stores.employees.repository.AddressRepository;
 import com.central.stores.employees.repository.EmployeesRepository;
 import com.central.stores.employees.test.utils.ClassBuilder;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
 public class AddressServicesImplentTest {
 	@InjectMocks
 	private AddressServicesImplent services;
+
+	@Mock
+	private Validator mockValidator;
 
 	@Mock
 	private AddressMapper mapper;
@@ -33,12 +46,16 @@ public class AddressServicesImplentTest {
 
 	@Mock
 	private EmployeesRepository employeesRepository;
-
+	
 	private Address address;
 
 	private Employee employee;
 
 	private AddressDTO requestAddressDTO;
+	
+	private LocalValidatorFactoryBean validator;
+	
+	private Set<ConstraintViolation<Object>> violations;
 
 	@BeforeEach
 	void setUp() {
@@ -46,22 +63,61 @@ public class AddressServicesImplentTest {
 		address = ClassBuilder.addressBuilder();
 		employee = ClassBuilder.employeeBuilder();
 		requestAddressDTO = ClassBuilder.addressDTOBuilder();
+		
+		validator = new LocalValidatorFactoryBean();
+		validator.afterPropertiesSet();
 	}
 
+	
 	@Test
-	void createTest() {
+	void create() {
 		when(mapper.toModel(any())).thenReturn(address);
 		when(employeesRepository.findById(any())).thenReturn(Optional.of(employee));
-		Address address = (Address) services.create(requestAddressDTO, UUID.randomUUID()).getBody();
+		ResponseEntity<Object> address = services.create(requestAddressDTO, UUID.randomUUID());
 		
-		assertNotNull(address);
+		assertTrue(address.getStatusCode().equals(HttpStatus.CREATED));
 	}
 	
 	@Test
-	void updateTest() {
-		when(addressRepository.findById(any())).thenReturn(Optional.of(address));
-		Address address = (Address) services.update(requestAddressDTO, UUID.randomUUID()).getBody();
+	void createWithMissingFields() {
+		requestAddressDTO.setNumber(null);
 		
-		assertNotNull(address);
+		violations = validator.validate(requestAddressDTO);
+		when(mockValidator.validate(any())).thenReturn(violations);
+		ResponseEntity<Object> address = services.create(requestAddressDTO, UUID.randomUUID());
+		
+		assertTrue(address.getStatusCode().equals(HttpStatus.UNPROCESSABLE_ENTITY));
+	}
+	
+	@Test
+	void update() {
+		when(addressRepository.findById(any())).thenReturn(Optional.of(address));
+		ResponseEntity<Object> address = services.update(requestAddressDTO, UUID.randomUUID());
+		
+		assertTrue(address.getStatusCode().equals(HttpStatus.NO_CONTENT));
+	}
+	
+	@Test
+	void updateWithMissingFields() {
+		requestAddressDTO.setNumber(null);
+		
+		violations = validator.validate(requestAddressDTO);
+		when(mockValidator.validate(any())).thenReturn(violations);
+		ResponseEntity<Object> address = services.update(requestAddressDTO, UUID.randomUUID());
+		
+		assertTrue(address.getStatusCode().equals(HttpStatus.UNPROCESSABLE_ENTITY));
+	}
+	
+	@Test
+	void updateResourceNotFoundException() {
+		String messageError = "No records found for this id";
+		
+		when(addressRepository.findById(any())).thenReturn(Optional.ofNullable(null));
+		
+		String message = assertThrows(ResourceNotFoundException.class, () -> {
+			services.update(requestAddressDTO, UUID.randomUUID());
+		}).getMessage();
+		
+		assertEquals(messageError, message);
 	}
 }
--- backend/CentralStoresEmployees/src/test/java/com/central/stores/employees/services/implement/EmployeesServicesImplementTest.java
+++ backend/CentralStoresEmployees/src/test/java/com/central/stores/employees/services/implement/EmployeesServicesImplementTest.java
@@ -2,16 +2,24 @@ package com.central.stores.employees.services.implement;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 import java.util.UUID;
 
+import javax.validation.ConstraintViolation;
+import javax.validation.Validator;
+
 import com.central.stores.employees.crypto.Cryptography;
+import com.central.stores.employees.exception.DuplicateDocumentsException;
+import com.central.stores.employees.exception.ResourceNotFoundException;
 import com.central.stores.employees.mapper.EmployeeMapper;
 import com.central.stores.employees.model.Employee;
 import com.central.stores.employees.model.dto.RequestEmployeeDTO;
@@ -24,6 +32,9 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 public class EmployeesServicesImplementTest {
 
@@ -31,27 +42,37 @@ public class EmployeesServicesImplementTest {
 	private EmployeesServicesImplement services;
 	
 	@Mock
-	private EmployeesRepository repository;
+	private EmployeeMapper mapper;
+
+	@Mock
+	private Validator mockValidator;
 	
 	@Mock
-	private EmployeeMapper mapper;
-	
+	private EmployeesRepository repository;
+
 	private Employee employee;
+
+	private LocalValidatorFactoryBean validator;
 
 	private RequestEmployeeDTO requestEmployeeDTO;
 	
 	private ResponseEmployeeDTO responseEmployeeDTO;
 	
+	private Set<ConstraintViolation<Object>> violations;
+
 	@BeforeEach
 	void setUp() {
 		MockitoAnnotations.openMocks(this);
 		employee = ClassBuilder.employeeBuilder();
 		requestEmployeeDTO = ClassBuilder.requestEmployeeDTOBuilder();
 		responseEmployeeDTO = ClassBuilder.responseEmployeeDTOBuilder();
+		
+		validator = new LocalValidatorFactoryBean();
+		validator.afterPropertiesSet();
 	}
 	
 	@Test
-	public void findByCpfTest() {
+	public void findByCpf() {
 		employee = Cryptography.encode(employee);
 		when(repository.findByCpf(anyString())).thenReturn(employee);
 		Employee emp = services.findByCpf("123456789");
@@ -60,7 +81,21 @@ public class EmployeesServicesImplementTest {
 	}
 	
 	@Test
-	public void findByNeighborhoodTest() {
+	public void findByCpfResourceNotFoundException() {
+		String messageError = "No record found for this cpf";
+		employee = Cryptography.encode(employee);
+		
+		when(repository.findByCpf(anyString())).thenReturn(null);
+		
+		String message = assertThrows(ResourceNotFoundException.class, () -> {
+			services.findByCpf("123456789");
+		}).getMessage();
+
+		assertEquals(messageError, message);
+	}
+	
+	@Test
+	public void findByNeighborhood() {
 		employee = Cryptography.encode(employee);
 		when(repository.findAllByActiveTrueAndAddressNeighborhood(anyString()))
 		.thenReturn(List.of(employee));
@@ -70,7 +105,21 @@ public class EmployeesServicesImplementTest {
 	}
 	
 	@Test
-	public void findAllTeste() {
+	public void findByNeighborhoodResourceNotFoundException() {
+		String messageError = "No record found for this neighborhood";
+		employee = Cryptography.encode(employee);
+		when(repository.findAllByActiveTrueAndAddressNeighborhood(anyString()))
+		.thenReturn(new ArrayList<Employee>());
+		
+		String message = assertThrows(ResourceNotFoundException.class, () -> {
+			services.findByNeighborhood("teste");
+		}).getMessage();
+		
+		assertEquals(messageError, message);
+	}
+	
+	@Test
+	public void findAll() {
 		employee = Cryptography.encode(employee);
 		when(repository.findAllByActiveTrue()).thenReturn(List.of(employee));
 		List<Employee> empList = services.findAll();
@@ -78,31 +127,124 @@ public class EmployeesServicesImplementTest {
 	}
 	
 	@Test
-	public void deleteTest() {
-		employee = Cryptography.encode(employee);
+	public void delete() {
 		employee.setActive(false);
+		employee = Cryptography.encode(employee);
 		when(mapper.employeeDelete(any())).thenReturn(employee);
 		when(repository.findById(any())).thenReturn(Optional.of(employee));
 		Employee emp = (Employee) services.delete(UUID.randomUUID());
 		
 		assertTrue(emp.getActive().equals(false));
 	}
-	
 	@Test
-	public void createTest() {
-		when(mapper.toModel(any())).thenReturn(employee);
-		when(mapper.modelToResponseEmployeeDTO(any())).thenReturn(responseEmployeeDTO);
-		RequestEmployeeDTO  emp = (RequestEmployeeDTO) services.create(requestEmployeeDTO).getBody();
+	public void deleteResourceNotFoundException() {
+		String messageError = "No records found for this id";
 		
-		assertNotNull(emp);
+		employee = Cryptography.encode(employee);
+		when(repository.findById(any())).thenReturn(Optional.ofNullable(null));
+		
+		String message = assertThrows(ResourceNotFoundException.class, () -> {
+			services.delete(UUID.randomUUID());
+		}).getMessage();
+		
+		assertEquals(messageError, message);
 	}
 	
 	@Test
-	public void updateTest() {
+	public void create() {
+		when(mapper.toModel(any())).thenReturn(employee);
+		when(mapper.modelToResponseEmployeeDTO(any())).thenReturn(responseEmployeeDTO);
+		ResponseEntity<Object> emp = services.create(requestEmployeeDTO);
+		
+		assertTrue(emp.getStatusCode().equals(HttpStatus.CREATED));
+	}
+	
+	@Test
+	public void createWithMissingFields() {
+		requestEmployeeDTO.setCpf(null);
+		
+		violations = validator.validate(requestEmployeeDTO);
+		when(mockValidator.validate(any())).thenReturn(violations);
+		ResponseEntity<Object> emp = services.create(requestEmployeeDTO);
+		
+		assertTrue(emp.getStatusCode().equals(HttpStatus.UNPROCESSABLE_ENTITY));
+	}
+	
+	@Test
+	public void createDuplicateDocumentsException() {
+		String messageError = "Documents already registered";
+		
+		when(mapper.toModel(any())).thenReturn(employee);
+		when(mapper.modelToResponseEmployeeDTO(any())).thenReturn(responseEmployeeDTO);
+		when(repository.findByRg(anyString())).thenReturn(employee);
+		when(repository.findByCpf(anyString())).thenReturn(employee);
+		
+		String message = assertThrows(DuplicateDocumentsException.class, () -> {
+			services.create(requestEmployeeDTO);
+		}).getMessage();
+		
+		assertEquals(messageError, message);
+	}
+	
+	@Test
+	public void createWithDuplicateCpf() {
+		String messageError = "Cpf already registered";
+		
+		when(mapper.toModel(any())).thenReturn(employee);
+		when(mapper.modelToResponseEmployeeDTO(any())).thenReturn(responseEmployeeDTO);
+		when(repository.findByCpf(anyString())).thenReturn(employee);
+		
+		String message = assertThrows(DuplicateDocumentsException.class, () -> {
+			services.create(requestEmployeeDTO);
+		}).getMessage();
+		
+		assertEquals(messageError, message);
+	}
+	@Test
+	public void createWithDuplicateRg() {
+		String messageError = "Rg already registered";
+		
+		when(mapper.toModel(any())).thenReturn(employee);
+		when(mapper.modelToResponseEmployeeDTO(any())).thenReturn(responseEmployeeDTO);
+		when(repository.findByRg(anyString())).thenReturn(employee);
+		
+		String message = assertThrows(DuplicateDocumentsException.class, () -> {
+			services.create(requestEmployeeDTO);
+		}).getMessage();
+		
+		assertEquals(messageError, message);
+	}
+	
+	@Test
+	public void update() {
 		when(repository.findById(any())).thenReturn(Optional.of(employee));
 		when(mapper.modelToResponseEmployeeDTO(any())).thenReturn(responseEmployeeDTO);
-		ResponseEmployeeDTO emp = (ResponseEmployeeDTO) services.update(requestEmployeeDTO, UUID.randomUUID()).getBody();
+		ResponseEntity<Object> emp = services.update(requestEmployeeDTO, UUID.randomUUID());
 		
-		assertNotNull(emp);
+		assertTrue(emp.getStatusCode().equals(HttpStatus.NO_CONTENT));
+	}
+
+	@Test
+	public void updateWithMissingFields() {
+		requestEmployeeDTO.setCpf(null);
+		
+		violations = validator.validate(requestEmployeeDTO);
+		when(mockValidator.validate(any())).thenReturn(violations);
+		ResponseEntity<Object> emp = services.update(requestEmployeeDTO, UUID.randomUUID());
+		
+		assertTrue(emp.getStatusCode().equals(HttpStatus.UNPROCESSABLE_ENTITY));
+	}
+	
+	@Test
+	public void updateResourceNotFoundException() {
+		String messageError = "No records found for this id";
+		
+		when(repository.findById(any())).thenReturn(Optional.ofNullable(null));
+		
+		String message = assertThrows(ResourceNotFoundException.class, () -> {
+			services.update(requestEmployeeDTO, UUID.randomUUID());
+		}).getMessage();
+		
+		assertEquals(messageError, message);
 	}
 }
